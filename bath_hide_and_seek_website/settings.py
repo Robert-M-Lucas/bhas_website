5f6c01159ee6fcd59d99dbc00d45e47780b42536
Robert-App-Dev <robert01.appdev@gmail.com>
2023-10-19 01:11:23 +0100

    Update

--- bath_hide_and_seek_website/settings.py
+++ bath_hide_and_seek_website/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+STATIC_ROOT = BASE_DIR / "collectedstatic"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
