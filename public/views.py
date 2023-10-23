09eb175a48b10cf1d04badf62105373d8b79a38f
Robert-App-Dev <robert01.appdev@gmail.com>
2023-10-23 08:46:34 +0100

    Update

--- public/views.py
+++ public/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
+from django.utils.timezone import now
 
+from backend.models import Event
 
 def index(request):
-    return render(request, 'public/index.html', {"pagename": "status"})
+    active_events = Event.objects.filter(start_time__lt=now(), end_time__gt=now(), deleted=False)
+    if len(active_events) == 0:
+        active_event = None
+    else:
+        active_event = active_events[0]
+
+    future_event = None
+    if active_event is None:
+        future_events = Event.objects.filter(start_time__gt=now(), deleted=False)
+        print(future_events)
+        if len(future_events) == 0:
+            future_event = None
+        else:
+            future_event = future_events[0]
+
+    return render(request, 'public/index.html',
+                  {
+                      "pagename": "status",
+                      "active_event": active_event,
+                      "future_event": future_event
+                  }
+                  )
 
 
 def status(request):
